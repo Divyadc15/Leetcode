e1d1da8f71ea8344eb98b48d8a7f27c05c8d7bcd
Divya Chaurasiya <divyachaurasiyadc@gmail.com>
2026-07-02 01:26:16 +0530

    [344] [Time Beats: 100.00%] [Memory Beats: 99.54%] - LeetPush

---
+++ Reverse-String.java
@@ -0,0 +1,17 @@
+1class Solution {
+2    public void reverseString(char[] s) {
+3        int i=0;
+4        int j=s.length-1;
+5        while(i<j){
+6            char temp=s[i];
+7            s[i]=s[j];
+8            s[j]=temp;
+9
+10            i=i+1;
+11            j=j-1;
+12        }
+13    }
+14}
+15
+16// TC=O(n)
+17// SC=O(1);
