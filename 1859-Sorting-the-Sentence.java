6707b56d5e74357bc8c472c367fd19aca4f0c53c
Divya Chaurasiya <divyachaurasiyadc@gmail.com>
2026-07-03 10:13:41 +0530

    [1859] [Time Beats: 42.99%] [Memory Beats: 66.26%] - LeetPush

---
+++ 1859-Sorting-the-Sentence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] ans = new String[words.length];
+
+        for (String word : words) {
+            int index = word.charAt(word.length() - 1) - '1';
+            ans[index] = word.substring(0, word.length() - 1);
+        }
+
+        String result = "";
+
+        for (int i = 0; i < ans.length; i++) {
+            result += ans[i];
+
+            if (i != ans.length - 1) {
+                result += " ";
+            }
+        }
+
+        return result;
+    }
+}
