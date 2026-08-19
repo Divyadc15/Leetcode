c5d7fa1e316d8709356b5f339998ff4158359bba
Divya Chaurasiya <divyachaurasiyadc@gmail.com>
2026-08-19 10:05:47 +0530

    [560] [Time Beats: 98.07%] [Memory Beats: 94.01%] - LeetPush

--- 560-Subarray-Sum-Equals-K.java
+++ 560-Subarray-Sum-Equals-K.java
@@ -1,24 +1,19 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int cnt = 0;
-        int[] pre = new int[nums.length];
-        pre[0] = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            pre[i] = pre[i - 1] + nums[i];
+        int n=nums.length;
+        int[] pre=new int[n+1];
+        pre[0]=0;
+        for (int i = 0; i < n; i++) {
+            pre[i + 1] = pre[i] + nums[i];
         }
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-
-        for (int i = 0; i < pre.length; i++) {
-            int required = pre[i] - k;
-
-            cnt += map.getOrDefault(required, 0);
-
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int ans=0;
+        map.put(0,1);
+        for(int i=1;i<=n;i++){
+            int pl=pre[i]-k;
+             ans += map.getOrDefault(pl, 0);
             map.put(pre[i], map.getOrDefault(pre[i], 0) + 1);
         }
-
-        return cnt;
+        return ans;
     }
 }
