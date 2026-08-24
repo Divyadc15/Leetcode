a6c23b54986b8f880db5cab18bb806d684993c92
Divya Chaurasiya <divyachaurasiyadc@gmail.com>
2026-08-24 09:29:46 +0530

    [69] [Time Beats: 98.43%] [Memory Beats: 42.99%] - LeetPush

--- 69-Sqrt(x).java
+++ 69-Sqrt(x).java
@@ -1,12 +1,11 @@
 class Solution {
     public int mySqrt(int x) {
-        long l=0,h=x;
+      long l=0,h=x;
       while(l<=h){
-          long mid=l+(h-l)/2;
-          if(mid*mid==x) return (int)mid;
-          else if(mid*mid>x) h=mid-1;
-          else
-          l=mid+1;
+        long mid=l+(h-l)/2;
+        if(mid*mid==x) return (int)mid;
+        else if(mid*mid>x) h=mid-1;
+        else l=mid+1;
       }
       return (int)h;
     }
