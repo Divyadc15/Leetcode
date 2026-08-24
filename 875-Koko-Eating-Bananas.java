9a85fcc2d2dbd7d3601d14ba29f70e5658a071bd
Divya Chaurasiya <divyachaurasiyadc@gmail.com>
2026-08-24 10:36:36 +0530

    [875] [Time Beats: 56.81%] [Memory Beats: 94.44%] - LeetPush

--- 875-Koko-Eating-Bananas.java
+++ 875-Koko-Eating-Bananas.java
@@ -1,22 +1,19 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l=1;
-        int r=Arrays.stream(piles).max().getAsInt();
-        int ans=r;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(canFinish(piles,h,mid)){
+        int l=1,hi=Arrays.stream(piles).max().getAsInt();
+        int ans=h;
+        while(l<=hi){
+            int mid=l+(hi-l)/2;
+            if(possilbe(piles,h,mid)==true){
                 ans=mid;
-                r=mid-1;
+                hi=mid-1;
             }
-            else{
-                l=mid+1;
-            }
+            else l=mid+1;
         }
         return ans;
     }
-    boolean canFinish(int[] piles,int h, int mid){
-        long  hours=0,n=piles.length;
+    boolean possilbe(int[]piles,int h,int mid){
+        long hours=0,n=piles.length;
         for(int i=0;i<n;i++){
             hours+=(piles[i]+mid-1)/mid;
         }
